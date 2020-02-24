779882cc30c4c5527700eb14b002232b17af9486
Andy Ni <nakukibo@gmail.com>
2020-02-24 14:32:42 -0600

    added gitignore

---
+++ app/src/main/java/me/nakukibo/colorbynumber/BitmapConversion.java
@@ -0,0 +1,109 @@
+package me.nakukibo.colorbynumber;
+
+import android.graphics.Bitmap;
+
+import androidx.core.graphics.ColorUtils;
+
+import java.util.LinkedList;
+
+class BitmapConversion {
+
+    /**
+     *
+     * */
+    static void convert(Bitmap photo){
+        boolean[][] changedColor = new boolean[photo.getHeight()][photo.getWidth()];
+
+        while(getNextFalseInArray(changedColor) != null){
+            setToAverage(photo, getNextFalseInArray(changedColor), changedColor);
+        }
+
+    }
+
+    private static void setToAverage(Bitmap photo, PixelCoordinates nextFalse, boolean[][] changedColor) {
+        int pixel = photo.getPixel(nextFalse.x, nextFalse.y);
+
+        changedColor[nextFalse.y][nextFalse.x] = true;
+
+        LinkedList<PixelCoordinates> surColors = new LinkedList<>();
+        LinkedList<PixelCoordinates> changedColors = new LinkedList<>();
+
+        surColors.add(new PixelCoordinates(nextFalse.x, nextFalse.y));
+        changedColors.add(new PixelCoordinates(nextFalse.x, nextFalse.y));
+
+        while(!surColors.isEmpty()){
+            PixelCoordinates pixelCoordinates = surColors.pop();
+            pushNeighbors(photo, surColors, changedColors, pixelCoordinates, pixel, changedColor);
+        }
+
+        while(!changedColors.isEmpty()){
+            PixelCoordinates pixelCoordinates = changedColors.pop();
+            photo.setPixel(pixelCoordinates.x, pixelCoordinates.y, pixel);
+        }
+    }
+
+    private static void pushNeighbors(Bitmap photo, LinkedList<PixelCoordinates> surColors, LinkedList<PixelCoordinates> changed,
+                                      PixelCoordinates curPixel,  int color, boolean[][] changedColor) {
+        int minX = curPixel.x > 0 ? curPixel.x - 1 : curPixel.x;
+        int maxX = curPixel.x < changedColor[0].length-1 ? curPixel.x + 1 : curPixel.x;
+
+        int minY = curPixel.y > 0 ? curPixel.y - 1 : curPixel.y;
+        int maxY = curPixel.y < changedColor.length-1 ? curPixel.y + 1 : curPixel.y;
+
+        for(int i=minY; i<=maxY; i++){
+            for(int j=minX; j<=maxX; j++){
+
+                if(!changedColor[i][j] && similarColors(color, photo.getPixel(j, i))) {
+                    changedColor[i][j] = true;
+                    surColors.add(new PixelCoordinates(j, i));
+                    changed.add(new PixelCoordinates(j, i));
+                }
+
+            }
+        }
+    }
+
+    // TODO: improve color comparison algorithm
+    private static boolean similarColors(int color, int pixel) {
+//        final int MAX_DISTANCE = 4000; //for rgb search
+        final double MAX_DISTANCE = 150.0; //lab search
+
+        double[] hsl1 = new double[3];
+        double[] hsl2 = new double[3];
+        ColorUtils.colorToLAB(color, hsl1);
+        ColorUtils.colorToLAB(pixel, hsl2);
+
+//        Log.d(TAG, "similarColors: hsl1 = " + hsl1[0] +", "+ hsl1[1] +", "+ hsl1[2]);
+//        Log.d(TAG, "similarColors: hsl2 = " + hsl2[0] +", "+ hsl2[1] +", "+ hsl2[2]);
+
+//        int r1 = Color.red(color);
+//        int g1 = Color.green(color);
+//        int b1 = Color.blue(color);
+//
+//        int r2 = Color.red(pixel);
+//        int g2 = Color.green(pixel);
+//        int b2 = Color.blue(pixel);
+
+//        return (r1-r2)*(r1-r2) + (g1-g2)*(g1-g2) + (b1-b2)*(b1-b2) < MAX_DISTANCE;
+        return (hsl1[0] - hsl2[0])*(hsl1[0] - hsl2[0]) + (hsl1[1] - hsl2[1])*(hsl1[1] - hsl2[1]) + (hsl1[2] - hsl2[2])*(hsl1[2] - hsl2[2]) <= MAX_DISTANCE;
+    }
+
+    private static PixelCoordinates getNextFalseInArray(boolean[][] booleanArray){
+        for(int i=0; i<booleanArray.length; i++){
+            for(int j=0; j<booleanArray[0].length; j++){
+                if(!booleanArray[i][j]) return new PixelCoordinates(j, i);
+            }
+        }
+        return null;
+    }
+
+    private static class PixelCoordinates {
+        private int x;
+        private int y;
+
+        PixelCoordinates(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
--- app/src/main/java/me/nakukibo/colorbynumber/MainActivity.java
+++ app/src/main/java/me/nakukibo/colorbynumber/MainActivity.java
@@ -5,16 +5,15 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.core.graphics.ColorUtils;
 
-import java.util.LinkedList;
+//TODO: reduce the minimum sdk required
 
 public class MainActivity extends Activity {
 
@@ -34,13 +33,15 @@ public class MainActivity extends Activity {
         imageViewRetrievedPhoto = findViewById(R.id.image_view_retrieved);
 
         Button buttonLaunchCamera = findViewById(R.id.button_launch_camera);
-        buttonLaunchCamera.setOnClickListener(v -> {
-
-            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
-            } else {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, REQUEST_CODE_FETCH_PHOTO);
+        buttonLaunchCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, REQUEST_CODE_FETCH_PHOTO);
+                }
             }
         });
     }
@@ -80,113 +81,9 @@ public class MainActivity extends Activity {
     }
 
     private void setPhoto(Bitmap photo){
-
-        boolean[][] changedColor = new boolean[photo.getHeight()][photo.getWidth()];
-
-        while(getNextFalseInArray(changedColor) != null){
-            setToAverage(photo, getNextFalseInArray(changedColor), changedColor);
-        }
-
+        BitmapConversion.convert(photo);
         imageViewRetrievedPhoto.setImageBitmap(photo);
     }
 
-    private void setToAverage(Bitmap photo, PixelCoordinates nextFalse, boolean[][] changedColor) {
-        int pixel = photo.getPixel(nextFalse.x, nextFalse.y);
 
-        changedColor[nextFalse.y][nextFalse.x] = true;
-
-        LinkedList<PixelCoordinates> surColors = new LinkedList<>();
-        LinkedList<PixelCoordinates> changedColors = new LinkedList<>();
-
-        surColors.add(new PixelCoordinates(nextFalse.x, nextFalse.y));
-        changedColors.add(new PixelCoordinates(nextFalse.x, nextFalse.y));
-
-        while(!surColors.isEmpty()){
-//            for(int i=0; i<changedColor.length; i++){
-//                StringBuilder stringBuilder = new StringBuilder();
-//
-//                for(int j=0; j<changedColor[0].length; j++){
-//                    stringBuilder.append(changedColor[i][j] ? "1":"0");
-//                }
-//
-//                Log.d(TAG, "setToAverage: " + stringBuilder.toString());
-//            }
-
-            PixelCoordinates pixelCoordinates = surColors.pop();
-            pushNeighbors(photo, surColors, changedColors, pixelCoordinates, pixel, changedColor);
-        }
-
-        while(!changedColors.isEmpty()){
-            PixelCoordinates pixelCoordinates = changedColors.pop();
-            photo.setPixel(pixelCoordinates.x, pixelCoordinates.y, pixel);
-        }
-    }
-
-    private void pushNeighbors(Bitmap photo, LinkedList<PixelCoordinates> surColors, LinkedList<PixelCoordinates> changed,
-                               PixelCoordinates curPixel,  int color, boolean[][] changedColor) {
-        int minX = curPixel.x > 0 ? curPixel.x - 1 : curPixel.x;
-        int maxX = curPixel.x < changedColor[0].length-1 ? curPixel.x + 1 : curPixel.x;
-
-        int minY = curPixel.y > 0 ? curPixel.y - 1 : curPixel.y;
-        int maxY = curPixel.y < changedColor.length-1 ? curPixel.y + 1 : curPixel.y;
-        Log.d(TAG, "pushNeighbors: " + String.format("minX: %d, maxX: %d, minY: %d, maxY: %d", minX, maxX, minY, maxY));
-
-        for(int i=minY; i<=maxY; i++){
-            for(int j=minX; j<=maxX; j++){
-
-                Log.d(TAG, "pushNeighbors: similar colors? " + (!changedColor[i][j]));
-                if(!changedColor[i][j] && similarColors(color, photo.getPixel(j, i))) {
-                    Log.d(TAG, "pushNeighbors: x=" + j + "y=" + i);
-                    changedColor[i][j] = true;
-                    surColors.add(new PixelCoordinates(j, i));
-                    changed.add(new PixelCoordinates(j, i));
-                }
-
-            }
-        }
-    }
-
-    // TODO: improve color comparison algorithm
-    private static boolean similarColors(int color, int pixel) {
-//        final int MAX_DISTANCE = 4000; //for rgb search
-        final double MAX_DISTANCE = 150.0; //lab search
-
-        double[] hsl1 = new double[3];
-        double[] hsl2 = new double[3];
-        ColorUtils.colorToLAB(color, hsl1);
-        ColorUtils.colorToLAB(pixel, hsl2);
-
-//        Log.d(TAG, "similarColors: hsl1 = " + hsl1[0] +", "+ hsl1[1] +", "+ hsl1[2]);
-//        Log.d(TAG, "similarColors: hsl2 = " + hsl2[0] +", "+ hsl2[1] +", "+ hsl2[2]);
-
-//        int r1 = Color.red(color);
-//        int g1 = Color.green(color);
-//        int b1 = Color.blue(color);
-//
-//        int r2 = Color.red(pixel);
-//        int g2 = Color.green(pixel);
-//        int b2 = Color.blue(pixel);
-
-//        return (r1-r2)*(r1-r2) + (g1-g2)*(g1-g2) + (b1-b2)*(b1-b2) < MAX_DISTANCE;
-        return (hsl1[0] - hsl2[0])*(hsl1[0] - hsl2[0]) + (hsl1[1] - hsl2[1])*(hsl1[1] - hsl2[1]) + (hsl1[2] - hsl2[2])*(hsl1[2] - hsl2[2]) <= MAX_DISTANCE;
-    }
-
-    private static PixelCoordinates getNextFalseInArray(boolean[][] booleanArray){
-        for(int i=0; i<booleanArray.length; i++){
-            for(int j=0; j<booleanArray[0].length; j++){
-                if(!booleanArray[i][j]) return new PixelCoordinates(j, i);
-            }
-        }
-        return null;
-    }
-
-    private static class PixelCoordinates {
-        private int x;
-        private int y;
-
-        PixelCoordinates(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
 }
