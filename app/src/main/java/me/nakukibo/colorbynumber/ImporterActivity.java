aba40b01245e8b5891d837b732d20b582b474f96
Andy Ni <nakukibo@gmail.com>
2020-03-03 11:58:11 -0600

    in progress attempt

--- app/src/main/java/me/nakukibo/colorbynumber/ImporterActivity.java
+++ app/src/main/java/me/nakukibo/colorbynumber/ImporterActivity.java
@@ -6,8 +6,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -16,6 +19,13 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class ImporterActivity extends Activity {
@@ -23,6 +33,8 @@ public class ImporterActivity extends Activity {
     private static final int REQUEST_CODE_CAMERA = 2;
 
     private ImageView imageViewRetrievedPhoto;
+
+    private String absoluteImageFilePath = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -68,32 +80,52 @@ public class ImporterActivity extends Activity {
 
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File tempPhotoFile;
+
+            try{
+                tempPhotoFile = createPhotoFile();
+
+            } catch (IOException e){
+                e.printStackTrace();
+                Toast.makeText(this, "Error: failed to create photo save file. Cannot proceed", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Uri photoUri = FileProvider.getUriForFile(this, "me.nakukibo.colorbynumber.provider", tempPhotoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
             startActivityForResult(takePictureIntent, REQUEST_CODE_FETCH_PHOTO);
         } else {
             Toast.makeText(this, "Unexpected error: failed to launch camera.", Toast.LENGTH_LONG).show();
         }
     }
 
+
+    private static final String TAG = "ImporterActivity";
+    private File createPhotoFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss-SS", Locale.US).format(new Date());
+        String fileName = "color-by-number_" + timeStamp;
+
+        File photoFile = File.createTempFile(fileName, ".jpg");
+        absoluteImageFilePath = photoFile.getAbsolutePath();
+
+        Log.d(TAG, "createPhotoFile: " + absoluteImageFilePath);
+
+        return photoFile;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
         super.onActivityResult(requestCode, resultCode, data);
 
         final String ERROR_NULL = "Unexpected error: failed to retrieve image.";
 
         if (requestCode == REQUEST_CODE_FETCH_PHOTO && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            if(extras == null){
-                Toast.makeText(this, ERROR_NULL, Toast.LENGTH_LONG).show();
-                return;
-            }
 
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            if(imageBitmap == null){
-                Toast.makeText(this, ERROR_NULL, Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            BitmapConversion.convert(imageBitmap);
+            Bitmap imageBitmap = BitmapFactory.decodeFile(absoluteImageFilePath);
+//            BitmapConversion.convert(imageBitmap);
             imageViewRetrievedPhoto.setImageBitmap(imageBitmap);
         } else {
             Toast.makeText(this, ERROR_NULL, Toast.LENGTH_LONG).show();
